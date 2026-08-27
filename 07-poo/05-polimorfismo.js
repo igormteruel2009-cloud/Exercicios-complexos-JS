b15b24e2c1be11125ba36096e00e0941ffcaf06f
TTRUEL <igormteruel2009@gmail.com>
2026-08-27 14:27:54 -0300

    Delete 07-poo directory

--- 07-poo/05-polimorfismo.js
+++
@@ -1,9 +0,0 @@
-class Funcionario{
-    constructor(nome,salario){
-        this.nome = nome;
-        this.salario = salario;
-    }
-    apresentar(){
-        console.log(`${this.nome} ganha de Salario: ${this.salario}`);
-    }
-}
